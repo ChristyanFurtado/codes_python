35377ce45111a1c643eed0b47c2868c81ce0d8ad
Christyan V. Rodrigues Furtado <christyan@gub.informatica>
2020-08-07 10:24:33 -0300

    Tralhando com listas em Python

---
+++ listas.py
@@ -0,0 +1,23 @@
+#Criação da lista
+#Lista de um único elemento
+listadecompras = ["toddy, leite, arroz, feijão, ovos, carne"]
+
+#Imprimir a lista
+print(listadecompras)
+
+#Outro formato de lista
+#Lista com mais elementos
+listabrasileirao = ["Flamengo", "Atlético-MG", "Grêmio", "Palmeiras", "São Paulo"]
+
+#Imprimir segunda lista
+print(listabrasileirao)
+
+#Imprimir apenas 1 elemento da lista
+print(listabrasileirao[2])
+
+#Alterando elemento da lista
+print(listabrasileirao)
+listabrasileirao[2] = listabrasileirao[3]
+listabrasileirao[3] = listabrasileirao[4]
+listabrasileirao[4] = "Gremio"
+print(listabrasileirao)
