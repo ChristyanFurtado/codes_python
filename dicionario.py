1c42c9bab837e58f9243c7387184ade313ff7545
Christyan V. Rodrigues Furtado <christyan@gub.informatica>
2020-08-07 16:27:08 -0300

    Comecando a trabalhar com Dicionarios

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+#Criação de dicionário
+
+campeoes_brasileiros = {"Palmeiras": 10, "Santos": 8, "Corinthians": 7, "Flamengo": 7, "São Paulo": 6}
+print(campeoes_brasileiros)
+print("------------------------------------------------------------------------------", "\n")
+
+print("Quantos títulos brasileiros o Flamengo tem:", campeoes_brasileiros["Flamengo"])
+print("---------------------------------------------", "\n")
+
