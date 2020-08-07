d41d6ff53910e1f9ffd32e65bee96451ec5b3040
Christyan V. Rodrigues Furtado <christyan@gub.informatica>
2020-08-07 11:15:46 -0300

    Atualizando o arquivo README e novas operacoes com listas

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # codes_python
 Códigos feitos durante o aprendizado da linguagem Python
+
+	# hello_word.py
+		- Primeiro código feito em python;
+		- Escolhi começar com o bom e velho "Hello Word".
+
+	# listas.py
+		- Começando a trabalhar com listas no python;
+		- Código contém algumas operações com listas em python:
+			* Criação de lista;
+			* Impressão de listas;
+			* Troca de elementos dentro de uma lista;
+			* Deletando elementos da lista;
+			* Adicionando item em lista existente;
+			* Listas aninhadas.
+
+
--- listas.py
+++ listas.py
@@ -5,6 +5,8 @@ listadecompras = ["toddy, leite, arroz, feijão, ovos, carne"]
 #Imprimir a lista
 print(listadecompras)
 
+print("---------------------------------------------------------------")
+
 #Outro formato de lista
 #Lista com mais elementos
 listabrasileirao = ["Flamengo", "Atlético-MG", "Grêmio", "Palmeiras", "São Paulo"]
@@ -12,8 +14,12 @@ listabrasileirao = ["Flamengo", "Atlético-MG", "Grêmio", "Palmeiras", "São Pa
 #Imprimir segunda lista
 print(listabrasileirao)
 
+print("---------------------------------------------------------------")
+
 #Imprimir apenas 1 elemento da lista
 print(listabrasileirao[2])
+
+print("---------------------------------------------------------------")
 
 #Alterando elemento da lista
 print(listabrasileirao)
@@ -21,3 +27,51 @@ listabrasileirao[2] = listabrasileirao[3]
 listabrasileirao[3] = listabrasileirao[4]
 listabrasileirao[4] = "Gremio"
 print(listabrasileirao)
+
+print("---------------------------------------------------------------")
+
+#Deletando elemento da lista
+print(listabrasileirao)
+del listabrasileirao[2]
+print(listabrasileirao)
+
+print("---------------------------------------------------------------")
+
+#Adicionando elemento na lista
+print(listabrasileirao)
+listabrasileirao.append("Palmeiras")
+listabrasileirao.append("Santos")
+print(listabrasileirao)
+
+print("---------------------------------------------------------------")
+
+#Listas aninhadas
+lista_brasileirao_2019 = [["Flamengo-RJ", "Santos-SP", "Palmeiras-SP", "Grêmio-RS"],
+                          ["Bragantino-SP", "Sport-PE", "Coritiba-PR", "Atlético-GO"],
+                          ["Náutico-PE", "Sampaio Correa - MA", "Juventude-RS", "Confiança-SE"],
+                          ["Brusque-SC", "Manaus-AM", "Ituano-SP", "Jacuipense-BA"]]
+print(lista_brasileirao_2019)
+print("---------------------------------------------------------------")
+#Separando listas aninhadas
+serie_a = lista_brasileirao_2019[0]
+serie_b = lista_brasileirao_2019[1]
+serie_c = lista_brasileirao_2019[2]
+serie_d = lista_brasileirao_2019[3]
+print("Os 4 primeiros colocados do Brasileirão 2019 - Série A: \n", serie_a, "\n")
+print("Os 4 primeiros colocados do Brasileirão 2019 - Série B: \n", serie_b, "\n")
+print("Os 4 primeiros colocados do Brasileirão 2019 - Série C: \n", serie_c, "\n")
+print("Os 4 primeiros colocados do Brasileirão 2019 - Série D: \n", serie_d, "\n")
+
+print("---------------------------------------------------------------")
+
+campeao_serieA = serie_a[0]
+campeao_serieB = serie_b[0]
+campeao_serieC = serie_c[0]
+campeao_serieD = serie_d[0]
+
+print("O grande campeão da série A 2019 foi: ", campeao_serieA, "\n")
+print("O grande campeão da série B 2019 foi: ", campeao_serieB, "\n")
+print("O grande campeão da série C 2019 foi: ", campeao_serieC, "\n")
+print("O grande campeão da série D 2019 foi: ", campeao_serieD, "\n")
+
+print("---------------------------------------------------------------")
